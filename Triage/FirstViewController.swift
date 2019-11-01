206b2e158f8d6a42deee19146f0f330cd77ee5ab
Francis Li <mail@francisli.com>
2019-11-01 13:36:04 -0700

    [#1] Initial project creation, CocoaPods integegration

---
+++ Triage/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Triage
+//
+//  Created by Francis Li on 11/1/19.
+//  Copyright © 2019 Francis Li. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Triage/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Triage
+//
+//  Created by Francis Li on 11/1/19.
+//  Copyright © 2019 Francis Li. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
