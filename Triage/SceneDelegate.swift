a8f5bdc772e16ec18a645d02df799b4f7bfda8ca
Francis Li <mail@francisli.com>
2026-09-24 17:50:40 -0500

    Update to Scene config

---
+++ Triage/SceneDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  SceneDelegate.swift
+//  Triage
+//
+//  Created by Francis Li on 9/24/26.
+//  Copyright © 2026 Francis Li. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+}
